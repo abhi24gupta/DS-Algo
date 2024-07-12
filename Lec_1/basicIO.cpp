e9f00e0fafc6e8874f8f14d6eca0366b22299657
Abhinav Gupta <abhinav.gupta@airtelbank.com>
2024-07-13 02:15:29 +0530

    Lec 1 Commits

---
+++ Lec_1/basicIO.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+cout<<"Hello World"<<endl;
+   
+   // Data Types
+    // int , long, long long, float, double
+    // string and getline
+    string str;
+    getline(cin,str);
+    cout<<str;
+    return 0;
+}
---
+++ Lec_1/ifElse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+// take age from user and print adult or not 
+    int age;
+    cin >> age;
+
+    if(age>=18){
+        cout<<"Adult"<<endl;
+    }else{
+        cout<<"Not Adult"<<endl;
+    }
+}
